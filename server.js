dda7e08c6a7ece98808da558413f218c947432e1
Dale Holstein <DaleHolstein@Dales-MacBook-Pro.local>
2018-02-02 21:53:07 -0500

    added config for mlab

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -26,9 +26,9 @@ app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB & Mongoose
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/WebdevScraper", {
-  useMongoClient: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/WebdevScraper"
+);
 
 // Routes
 
